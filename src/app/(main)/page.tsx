5681c5d9058d56c19764f4b75b2b086d0687f676
mohits-git <mohitkhatri2805@gmail.com>
2024-05-17 23:07:12 +0530

    feat: added basic auth in root page for redirects and handling new Users

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,27 @@
-import { ModeToggle } from "@/components/mode-toggle";
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+import Unauthorized from "@/components/global/unauthorized";
+import { getAuthUserDetails, verifyUserAndInvitation } from "@/lib/queries";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const agencyId = await verifyUserAndInvitation();
+  const user = await getAuthUserDetails();
+
+  if (agencyId) {
+    if (user?.role === "TEAM_MEMBER") {
+      return redirect("/project");
+    } else if (user?.role === "AGENCY_OWNER") {
+      return redirect(`/${agencyId}`);
+    } else {
+      return <Unauthorized />;
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
-      <Button>Click Me!!</Button>
-      <UserButton />
-      <ModeToggle />
+      <h1>Get Started</h1>
+      <h2>Create Your Agency</h2>
+      {/* TODO: Add agency form */}
+      {/* AgencyDetails */}
     </main>
   );
 }
