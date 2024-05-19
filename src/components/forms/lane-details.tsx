86a979ea7d1dfbb719a66b1fb53be8bbd9723d42
mohits-git <mohitkhatri2805@gmail.com>
2024-05-19 20:07:10 +0530

    feat: added LaneDetails form

---
+++ src/components/forms/lane-details.tsx
@@ -0,0 +1,130 @@
+'use client'
+import React, { useEffect } from 'react'
+import { z } from 'zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+} from '@/components/ui/card'
+import { useForm } from 'react-hook-form'
+import { Lane } from '@prisma/client'
+import { Input } from '../ui/input'
+
+import { Button } from '../ui/button'
+import Loading from '../global/loading'
+import {
+  upsertLane,
+} from '@/lib/queries'
+import { toast } from 'sonner'
+import { useModal } from '@/providers/modal-provider'
+import { useRouter } from 'next/navigation'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+interface CreateLaneDetailsProps {
+  defaultData?: Lane
+  projectId: string
+}
+
+const LaneFormSchema = z.object({
+  name: z.string().min(1),
+});
+
+const LaneDetails: React.FC<CreateLaneDetailsProps> = ({
+  defaultData,
+  projectId,
+}) => {
+  const { setClose } = useModal()
+  const router = useRouter()
+  const form = useForm<z.infer<typeof LaneFormSchema>>({
+    mode: 'onChange',
+    resolver: zodResolver(LaneFormSchema),
+    defaultValues: {
+      name: defaultData?.name || '',
+    },
+  })
+
+  useEffect(() => {
+    if (defaultData) {
+      form.reset({
+        name: defaultData.name || '',
+      })
+    }
+  }, [defaultData])
+
+  const isLoading = form.formState.isLoading
+
+  const onSubmit = async (values: z.infer<typeof LaneFormSchema>) => {
+    if (!projectId) return
+    try {
+      const response = await upsertLane({
+        ...values,
+        id: defaultData?.id,
+        projectId: projectId,
+        order: defaultData?.order,
+      });
+
+      toast('Success', {
+        description: 'Saved List details',
+      });
+
+      router.refresh()
+    } catch (error) {
+      toast('Oppse!', {
+        description: 'Could not save list details',
+      });
+    }
+    setClose()
+  }
+  return (
+    <Card className="w-full ">
+      <CardHeader>
+        <CardTitle>List Details</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-4"
+          >
+            <FormField
+              disabled={isLoading}
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>List Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="List Name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button
+              className="w-20 mt-4"
+              disabled={isLoading}
+              type="submit"
+            >
+              {form.formState.isSubmitting ? <Loading /> : 'Save'}
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default LaneDetails;
