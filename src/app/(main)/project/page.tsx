176ad64c36fc02456aeb50ad2030dcfe870e9cd0
mohits-git <mohitkhatri2805@gmail.com>
2024-05-22 20:40:15 +0530

    feat: added UserButton for unauthorized page

--- src/app/(main)/project/page.tsx
+++ src/app/(main)/project/page.tsx
@@ -1,6 +1,7 @@
 import ProjectDetails from "@/components/forms/project-details";
 import Unauthorized from "@/components/global/unauthorized";
 import { getAuthUserDetails, verifyUserAndInvitation } from "@/lib/queries";
+import { UserButton } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -24,7 +25,17 @@ const Page: React.FC = async () => {
     );
   }
 
-  return <Unauthorized />
+  return (
+    <div className="p-4 text-center h-screen w-screen flex items-center justify-center flex-col">
+      <h1 className="text-3xl md:text-6xl">No Project Access</h1>
+      <p className="max-w-xl mt-2">Please contact your Agency Owner to grant access to a Project or Login with different emailId.</p>
+      <div className="my-3 text-xl font-semibold flex items-center gap-3">
+        <span>User Profile: </span>
+        <UserButton />
+      </div>
+    </div>
+
+  )
 }
 
 export default Page
--- src/components/global/unauthorized.tsx
+++ src/components/global/unauthorized.tsx
@@ -1,16 +1,21 @@
+import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function Unauthorized() {
   return (
     <div className="p-4 text-center h-screen w-screen flex items-center justify-center flex-col">
       <h1 className="text-3xl md:text-6xl">Unauthorized Access</h1>
-      <p>Please login with different emailId or get an invitation from your Agency Owner.</p>
+      <p className="max-w-xl mt-2">Please login with different emailId or get an invitation from your Agency Owner.</p>
       <Link
         href={'/'}
-        className="mt-4 p-2 bg-primary"
+        className="mt-4 p-2 bg-secondary rounded-lg hover:bg-secondary/50"
       >
         Back to Home
       </Link>
+      <div className="my-3 text-xl font-semibold flex items-center gap-3">
+        <span>User Profile: </span>
+        <UserButton />
+      </div>
     </div>
   )
 }
